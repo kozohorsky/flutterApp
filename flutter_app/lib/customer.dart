5de4d6a4e7292cb609ae56de3d7049344140ce8d
kozohorsky <kozohorsky@live.com>
2021-04-29 18:37:29 +0200

    last commit

---
+++ flutter_app/lib/customer.dart
@@ -0,0 +1,18 @@
+class Customer {
+  String name;
+  String surname;
+  String address;
+  String email;
+  String phone;
+  String photo;
+  int balance;
+
+  Customer(
+      {this.name,
+      this.surname,
+      this.address,
+      this.email,
+      this.phone,
+      this.photo,
+      this.balance});
+}
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,8 +1,49 @@
+import 'dart:io';
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'customer.dart';
 
 void main() => runApp(MaterialApp(home: Home()));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  List<Customer> customers = [
+    Customer(
+        name: 'Vladimir',
+        surname: 'Kozohorsky',
+        address: 'Praha 10',
+        email: 'kozv01@vse.cz',
+        phone: '777 233 539',
+        photo: 'avatar.png',
+        balance: 1000),
+    Customer(
+        name: 'Monika',
+        surname: 'Kozohorska',
+        address: 'Praha 10',
+        email: 'monika.k@seznam.cz',
+        phone: '777 233 538',
+        photo: 'stockapp.png',
+        balance: 20)
+  ];
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int cash = 1000;
+  File _image;
+  final imagePicker = ImagePicker();
+  Future getImage() async {
+    final image = await imagePicker.getImage(source: ImageSource.camera);
+    setState(() {
+      
+      _image = File(image.path);
+  
+    });
+    log(_image.path);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,15 +52,28 @@ class Home extends StatelessWidget {
         title: Text('User Profile'),
         backgroundColor: Colors.grey[800],
       ),
-      body: Padding(
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.grey,
+          onPressed: getImage,
+          child: Icon(Icons.camera_alt)),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 30.0),
+        child:       Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
           children: <Widget>[
             Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/avatar.png'),
-                radius: 60.0,
-              ),
+             child: Container(
+               padding: EdgeInsets.fromLTRB(60.0, 10.0, 60.0, 0.0),
+  child: _image == null ? Text("Fotka neyla vybrana") : Image.file(_image)
+)
+             // child: CircleAvatar(
+              //  backgroundImage: _image == null
+              //      ? AssetImage('assets/avatar.png')
+              //      : AssetImage(_image.path),
+              //  radius: 60.0,
+             // ),
+              //child: _image == null ? Text("Fotka neyla vybrana") : Image.file(_image)
             ),
             SizedBox(height: 40.0),
             Row(
@@ -98,9 +152,24 @@ class Home extends StatelessWidget {
                         fontSize: 20.0))
               ],
             ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.money, color: Colors.green),
+                SizedBox(width: 10.0),
+                Text('balance: $cash czk',
+                    style: TextStyle(
+                        color: Colors.amber[300],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0))
+              ],
+            ),
           ],
         ),
       ),
+      ),
+
     );
   }
 }
